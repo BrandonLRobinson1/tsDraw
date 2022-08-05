eb637abc6d8287dcf841759bfd79383874f324ec
Brandon Robinson <BrandonLRobinson1@gmail.com>
2022-08-05 19:01:46 -0400

    basic structure of welcom page, and path to server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+// const path = require("path");
 
 const app = express();
+
+// serve static files
+// app.use(express.static(path.join(__dirname, "../client", "build")));
+// app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3100;
 
